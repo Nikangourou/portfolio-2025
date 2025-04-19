da5c23655d568e283ab6d27a2747ad5e9b3edc17
nicolas <nicolas.neveu06@gmail.com>
2025-04-19 15:58:33 +0200

    remove rotate

--- src/components/CameraController.jsx
+++ src/components/CameraController.jsx
@@ -1,28 +1,15 @@
 import { useThree, useFrame } from '@react-three/fiber';
-import { useRef, useEffect } from 'react';
+import { useRef } from 'react';
 import * as THREE from 'three';
 
 export default function CameraController() {
   const { camera } = useThree();
-  const scrollAngle = useRef(0);
   const target = new THREE.Vector3(0, 0, -5);
-  const radius = 5;
-
-  useEffect(() => {
-    const handleWheel = (event) => {
-      event.preventDefault(); // Empêche le scroll natif
-      scrollAngle.current += event.deltaY * 0.005;
-    };
-    window.addEventListener('wheel', handleWheel, { passive: false });
-    return () => window.removeEventListener('wheel', handleWheel);
-  }, []);
 
   useFrame(() => {
-    const angle = scrollAngle.current;
-    camera.position.x = target.x + radius * Math.sin(angle);
-    // camera.position.y = target.y + radius * Math.sin(angle * 0.3);
-    camera.position.z = target.z + radius * Math.cos(angle);
+    camera.position.set(0, 0, 0);
     camera.lookAt(target);
   });
+  
   return null;
 } 
