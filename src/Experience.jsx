c64f1fb9ffe4ae5106ac74ab27a69d94d116d41f
nicolas <nicolas.neveu06@gmail.com>
2025-08-11 02:26:08 +0200

    Simplify camera

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,12 +1,7 @@
 import Projects from './components/Projets/Projects.jsx'
-import CameraController from './components/CameraController.jsx'
-
 
 export default function Experience() {
   return (
-    <>
-      <CameraController />
-      <Projects />
-    </>
+    <Projects />
   )
 }
