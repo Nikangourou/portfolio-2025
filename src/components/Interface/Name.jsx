690e49aa874eb0f909a3fe32426744dd1c2937e5
nicolas <nicolas.neveu06@gmail.com>
2025-08-25 18:02:06 +0200

    remove name

--- src/components/Interface/Name.jsx
+++ src/components/Interface/Name.jsx
@@ -1,26 +1,51 @@
 import React from 'react';
+import { useSpring, animated, config } from '@react-spring/web';
 import useThemeStore from '../../stores/themeStore';
+import { useStore } from '../../stores/store';
 
 export default function Name() {
-  const currentTheme = useThemeStore((state) => state.currentTheme);
+  const isProjectsArranged = useStore((state) => state.isProjectsArranged);
 
+  // Animation pour faire disparaître le nom
+  const [springs, api] = useSpring(() => ({
+    opacity: 1,
+    y: 0,
+    config: config.gentle
+  }));
+
+  // Animation basée sur l'état des projets
+  React.useEffect(() => {
+    if (isProjectsArranged) {
+      // Disparaître quand les projets sont arrangés
+      api.start({
+        opacity: 0,
+        y: -20
+      });
+    } else {
+      // Réapparaître quand les projets ne sont pas arrangés
+      api.start({
+        opacity: 1,
+        y: 0
+      });
+    }
+  }, [isProjectsArranged, api]);
 
   return (
-    <div 
+    <animated.div 
       className="name"
       style={{
         position: 'fixed',
         top: '20px',
         left: '30px',
         zIndex: 1000,
-        color: currentTheme.text,
         fontSize: '1.5em',
         pointerEvents: 'none',
-        transition: 'color 1s ease',
         fontFamily: 'Allerta Stencil, sans-serif',
+        opacity: springs.opacity,
+        transform: springs.y.to(y => `translateY(${y}px)`)
       }}
     >
       Nicolas Neveu
-    </div>
+    </animated.div>
   );
 } 
